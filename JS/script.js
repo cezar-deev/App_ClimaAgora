02eafc649f32f5cc04dddff4fb34f29bcb30644d
Cezar Cardoso <cezar.deev@gmail.com>
2022-09-22 15:26:40 -0300

    CSS quase pronto

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,3 @@
 // chave : 0e0343ae7f6c8b7caade1935eeaecdab //
+
+// {"coord":{"lon":10.99,"lat":44.34},"weather":[{"id":801,"main":"Clouds","description":"few clouds","icon":"02n"}],"base":"stations","main":{"temp":285.82,"feels_like":284.87,"temp_min":283.67,"temp_max":287.16,"pressure":1021,"humidity":66,"sea_level":1021,"grnd_level":934},"visibility":10000,"wind":{"speed":1.61,"deg":141,"gust":3.47},"clouds":{"all":13},"dt":1663867840,"sys":{"type":2,"id":2004688,"country":"IT","sunrise":1663822972,"sunset":1663866893},"timezone":7200,"id":3163858,"name":"Zocca","cod":200} //
