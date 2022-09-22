d0ba140232896740a7410eb1a65a54c3292ce488
Cezar Cardoso <cezar.deev@gmail.com>
2022-09-22 16:59:48 -0300

    Atualizar

---
+++ JS/scripts.js
@@ -0,0 +1,22 @@
+
+
+// Variavesi e seleção de elementos
+const apikey ="0e0343ae7f6c8b7caade1935eeaecdab";
+const apiCountryURL = "https://countryflagsapi.com/png/"; // puxa a bandeira
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+
+// Funções 
+
+
+
+
+// Eventos
+
+searchBtn.addEventListener("click", (e) => { 
+    e.preventDefault();
+
+    console.log("teste");
+});     
